497fa2989cd5b5f5209fc7e09103410a9303fbfa
Mouriya <mouriyavk.official@gmail.com>
2025-04-20 12:02:01 +0530

    adding frontend with react nav bar

---
+++ frontend/src/components/pages/Events.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>event</div>
+  )
+}
+
+export default Events;
---
+++ frontend/src/components/pages/Leaderboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Leaderboard = () => {
+  return (
+    <div>leader board</div>
+  )
+}
+
+export default Leaderboard;
